f49bbcee3c9e1d5547cd47d511bcdf4465b48773
Vladislav Shilov <skilleton2012@gmail.com>
2017-04-17 00:01:11 +0300

    Initial Commit

---
+++ itsImportant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  itsImportant
+//
+//  Created by Vladislav Shilov on 17.04.17.
+//  Copyright © 2017 Vladislav Shilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
